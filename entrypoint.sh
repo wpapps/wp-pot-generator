48286a6d3819e0fc4c020c3bc5c00056bcf79e3d
Varun Sridharan <varunsridharan23@gmail.com>
2019-04-01 06:45:28 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Note that this does not use pipefail
+# because if the grep later doesn't match any deleted files,
+# which is likely the majority case,
+# it does not exit with a 0, and I only care about the final exit.
+set -eo
