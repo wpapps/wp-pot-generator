9ec31ad7b1ee08ee8bb5dd76d3ab4869077ebc9a
Varun Sridharan <varunsridharan23@gmail.com>
2019-04-05 15:34:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,20 @@ if [[ -z "$SLUG" ]]; then
 	SLUG=${GITHUB_REPOSITORY#*/}
 fi
 
-if [[ -z "$destination" ]]; then
+if [[ -z "$SAVE_PATH" ]]; then
 	echo "Set Pot File Save destination"
+	exit 1
+fi
+
+if [[ -z "$DOMAIN" ]]; then
+	echo "Set Text Domain"
+	exit 1
+fi
+
+
+
+if [[ -z "$USER" ]]; then
+	echo "Set Text User (WordPress.org username / wordpress.org email)"
 	exit 1
 fi
 
@@ -26,6 +38,6 @@ echo "ℹ︎ SLUG is $SLUG"
 
 cd "$GITHUB_WORKSPACE"
 ls -l
-wp i18n make-pot . my-plugin.pot --allow-root
+wp i18n make-pot . $SAVE_PATH --allow-root --user $USER
 ls -l
-cat my-plugin.pot
+cat $SAVE_PATH
