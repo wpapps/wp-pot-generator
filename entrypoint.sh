655ecc7073e0416e7f4ce60aea8b7c1a0fa84b93
Varun Sridharan <varunsridharan23@gmail.com>
2019-04-05 18:47:49 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@
 set -eo
 
 # Allow some ENV variables to be customized
-if [[ -z "$SLUG" ]]; then
+if [[ -z "$ITEM_SLUG" ]]; then
 	SLUG=${GITHUB_REPOSITORY#*/}
 fi
 
@@ -22,14 +22,15 @@ if [[ -z "$SAVE_PATH" ]]; then
 fi
 
 if [[ -z "$DOMAIN" ]]; then
-	echo "Set Text Domain"
-	exit 1
+	#echo "Set Text Domain"
+	#exit 1
+	DOMAIN = $ITEM_SLUG
 fi
 
-if [[ -z "$USER" ]]; then
-	echo "Set Text User (WordPress.org username / wordpress.org email)"
-	exit 1
-fi
+#if [[ -z "$USER" ]]; then
+#	echo "Set Text User (WordPress.org username / wordpress.org email)"
+#	exit 1
+#fi
 
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "Set the GITHUB_TOKEN env variable"
@@ -37,19 +38,24 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 fi
 
 if [[ -z "$HEADERS" ]]; then
-	echo "Set the GITHUB_TOKEN env variable"
-	exit 1
+	HEADERS = ''
 fi
 
-git config --global user.email "wppotgenerator+github@gmail.com" \
-&& git config --global user.name "WordPress Pot Generator on GitHub"
+echo $ITEM_SLUG
+echo $DOMAIN
+
+## Update Git Config.
+git config --global user.email "wppotgenerator+github@gmail.com" && git config --global user.name "WordPress Pot Generator on GitHub"
 
 cd "$GITHUB_WORKSPACE"
-wp i18n make-pot . "$SAVE_PATH" --user="$USER" --slug="$SLUG" --package-name="$PACKAGE_NAME" --headers="$HEADERS" --allow-root
-# ls -l
-# cat $SAVE_PATH
-# echo ${GITHUB_REF}
-# echo $GITHUB_REF
+
+# Generate POT File
+#wp i18n make-pot . "$SAVE_PATH" --user="$USER" --slug="$SLUG" --package-name="$PACKAGE_NAME" --headers="$HEADERS" --allow-root
+wp i18n make-pot . "$SAVE_PATH" --slug="$ITEM_SLUG" --package-name="$PACKAGE_NAME" --headers="$HEADERS" --allow-root
+
+# ls -l | cat $SAVE_PATH | echo ${GITHUB_REF} | echo $GITHUB_REF
+
+# Update In Github
 git add -A
 git commit -m "Pot File Updated"
 git remote set-url origin https://$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY}.git
